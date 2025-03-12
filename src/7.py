9bed7890e1aff8c8a61f7dcd97e86e5451160c9b
donarduttweiler523 <donarduttweiler523@gmail.com>
2025-03-12 10:31:09 +0000

    Add 7.py

---
+++ src/7.py
@@ -0,0 +1,11 @@
+
+def calculate_bmi(weight, height):
+    bmi = weight / (height ** 2)
+    if bmi < 18.5:
+        return "Underweight"
+    elif 18.5 <= bmi < 25:
+        return "Normal"
+    elif 25 <= bmi < 30:
+        return "Overweight"
+    else:
+        return "Obese"
